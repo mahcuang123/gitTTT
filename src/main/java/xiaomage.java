f47f95c391de80ac61a699a97b48575c7355e650
liuhuan <837444118@qq.com>
2020-03-23 21:08:52 +0800

    主分支提交

--- src/main/java/xiaomage.java
+++ src/main/java/xiaomage.java
@@ -1,6 +1,8 @@
 public class xiaomage {
     public static void main(String[] args) {
+        System.out.println("小马哥的主分支");
      System.out.println("小马哥的DEV分支");
+     String change="主分支内容更新";
     }
 
 }
