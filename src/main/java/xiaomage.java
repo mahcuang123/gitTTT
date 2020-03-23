dc0bdb11acfeb4bf90b9d19c861a313ef276e9f5
liuhuan <837444118@qq.com>
2020-03-23 23:17:17 +0800

    machuang

--- src/main/java/xiaomage.java
+++ src/main/java/xiaomage.java
@@ -2,9 +2,7 @@ public class xiaomage {
     public static void main(String[] args) {
 
 
-        System.out.println("小马哥的主分支");
-                System.out.println("我是老大");
-                System.out.println("我是老大");
+        System.out.println("machuang");
 
     }
 
