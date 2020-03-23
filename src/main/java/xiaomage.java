1bb2dfdf2c76f1ac123ce94aa3e9bb1f571bd5d6
liuhuan <837444118@qq.com>
2020-03-23 21:10:26 +0800

    dev分支

--- src/main/java/xiaomage.java
+++ src/main/java/xiaomage.java
@@ -1,6 +1,8 @@
 public class xiaomage {
     public static void main(String[] args) {
      System.out.println("小马哥的DEV分支");
+        System.out.println("dev分支的提交");
+        String dev = "我的名字是Dev";
     }
 
 }
