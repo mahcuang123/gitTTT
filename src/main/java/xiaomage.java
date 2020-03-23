02e1748b05091f4456732328d968ead361d9bf72
liuhuan <837444118@qq.com>
2020-03-23 23:12:14 +0800

    提价

--- src/main/java/xiaomage.java
+++ src/main/java/xiaomage.java
@@ -1,8 +1,6 @@
 public class xiaomage {
     public static void main(String[] args) {
 
-        System.out.println("小马哥的sss分支");
-        System.out.println("我是老大的儿子");
 
         System.out.println("小马哥的主分支");
                 System.out.println("我是老大");
